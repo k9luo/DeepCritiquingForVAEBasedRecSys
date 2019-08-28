715f298d0cc2360cff1b44de1dbf239b71bbdee0
k9luo <lucas.luokai@gmail.com>
2019-08-27 22:45:54 -0400

    added more helper function

--- utils/io.py
+++ utils/io.py
@@ -1,5 +1,20 @@
 from scipy.sparse import save_npz, load_npz
 
+import yaml
+
 
 def load_numpy(path, name):
     return load_npz(path+name).tocsr()
+
+def load_dataframe_csv(path, name):
+    return pd.read_csv(path+name)
+
+def load_yaml(path, key='parameters'):
+    with open(path, 'r') as stream:
+        try:
+            return yaml.load(stream)[key]
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def save_dataframe_csv(df, path, name):
+    df.to_csv(path+name, index=False)
