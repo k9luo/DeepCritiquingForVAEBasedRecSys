add95a98c66ebeed61928ccecf68bd03cf36dc7f
k9luo <lucas.luokai@gmail.com>
2019-10-24 16:57:43 -0400

    added helper functions

--- utils/io.py
+++ utils/io.py
@@ -1,5 +1,8 @@
 from scipy.sparse import save_npz, load_npz
 
+import ast
+import os
+import pandas as pd
 import yaml
 
 
@@ -18,3 +21,16 @@ def load_yaml(path, key='parameters'):
 
 def save_dataframe_csv(df, path, name):
     df.to_csv(path+name, index=False)
+
+def find_best_hyperparameters(folder_path, metric):
+    csv_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path)
+                 if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('.csv')]
+    best_settings = []
+    for record in csv_files:
+        df = pd.read_csv(record)
+        df[metric+'_Score'] = df[metric].map(lambda x: ast.literal_eval(x)[0])
+        best_settings.append(df.loc[df[metric+'_Score'].idxmax()].to_dict())
+
+    df = pd.DataFrame(best_settings).drop(metric+'_Score', axis=1)
+
+    return df
