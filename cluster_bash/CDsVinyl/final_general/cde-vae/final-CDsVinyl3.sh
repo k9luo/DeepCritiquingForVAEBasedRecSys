1825ae9a59414f73fbdc792a37e92831f3dd204b
k9luo <lucas.luokai@gmail.com>
2019-10-27 19:19:08 -0400

    updated cluster bash for final recommendation performance

---
+++ cluster_bash/CDsVinyl/final_general/cde-vae/final-CDsVinyl3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ~/ENV/bin/activate
+cd ~/Dual-Encoder
+python reproduce_general_results.py --data_dir data/CDsVinyl/ --tuning_result_path CDsVinyl --save_path CDsVinyl/CDsVinyl_final/CDsVinyl_final_result3.csv
---
+++ cluster_bash/beer/final_general/e-cde-vae/final-beer3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ~/ENV/bin/activate
+cd ~/Dual-Encoder
+python reproduce_general_results.py --data_dir data/beer/ --tuning_result_path beer_rating_tuning --save_path beer_rating_final/beer_final_result3.csv
