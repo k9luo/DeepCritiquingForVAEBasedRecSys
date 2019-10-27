removed keyphrase normalization for now and fixed the corresponding matrix type.

main.py
@@ -36,7 +36,6 @@ def main(args):
     print("Predict Batch Size: {}".format(args.predict_batch_size))
     print("Evaluation Ranking Topk: {}".format(args.topk))
     print("Validation Enabled: {}".format(args.enable_validation))
-    print("Binarize Keyphrase Frequency: {}".format(args.enable_keyphrase_binarization))
 
     # Load Data
     progress.section("Load Data")
@@ -50,27 +49,17 @@ def main(args):
 
     if args.enable_validation:
         R_valid = load_numpy(path=args.data_dir, name=args.valid_set)
-        R_valid_keyphrase = load_numpy(path=args.data_dir, name=args.valid_keyphrase_set).toarray()
+        R_valid_keyphrase = load_numpy(path=args.data_dir, name=args.valid_keyphrase_set)
     else:
         R_valid = load_numpy(path=args.data_dir, name=args.test_set)
-        R_valid_keyphrase = load_numpy(path=args.data_dir, name=args.test_keyphrase_set).toarray()
+        R_valid_keyphrase = load_numpy(path=args.data_dir, name=args.test_keyphrase_set)
     print("Elapsed: {}".format(inhour(time.time() - start_time)))
 
     progress.section("Preprocess Keyphrase Frequency")
     start_time = time.time()
 
-    if args.enable_keyphrase_binarization:
-        R_train_keyphrase[R_train_keyphrase != 0] = 1
-        R_valid_keyphrase[R_valid_keyphrase != 0] = 1
-    else:
-        R_train_keyphrase = R_train_keyphrase/R_train_keyphrase.sum(axis=1, keepdims=True)
-        R_valid_keyphrase = R_valid_keyphrase/R_valid_keyphrase.sum(axis=1, keepdims=True)
-
-        R_train_keyphrase[np.isnan(R_train_keyphrase)] = 0
-        R_valid_keyphrase[np.isnan(R_valid_keyphrase)] = 0
-
-#    import ipdb; ipdb.set_trace()
-
+    R_train_keyphrase[R_train_keyphrase != 0] = 1
+    R_valid_keyphrase[R_valid_keyphrase != 0] = 1
     print("Elapsed: {}".format(inhour(time.time() - start_time)))
 
     progress.section("Train")
@@ -165,10 +154,6 @@ if __name__ == "__main__":
 
     parser.add_argument('--model', dest='model', default="CDE-VAE",
                         help='Model currently using. (default: %(default)s)')
-
-    parser.add_argument('--normalize_keyphrase_frequency', dest='enable_keyphrase_binarization',
-                        action='store_false',
-                        help='Boolean flag indicating if keyphrase frequency is binarized.')
 
     parser.add_argument('--optimizer', dest='optimizer', default="Adam",
                         help='Optimizer currently using. (default: %(default)s)')