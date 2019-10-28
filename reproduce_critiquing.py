d9f6fc9358b482720a8c9fbc98d14bad4b17cc5b
k9luo <lucas.luokai@gmail.com>
2019-10-28 18:37:01 -0400

    Select top most frequent keyphrases for each item as an approximation for item's description

--- reproduce_critiquing.py
+++ reproduce_critiquing.py
@@ -3,20 +3,35 @@ from utils.argcheck import check_int_positive
 from utils.io import load_numpy
 
 import argparse
+import numpy as np
+import scipy.sparse as sparse
 
 
 def main(args):
 
     R_train = load_numpy(path=args.data_dir, name=args.train_set)
-    R_train_keyphrase = load_numpy(path=args.data_dir, name=args.train_keyphrase_set)
+    R_train_keyphrase = load_numpy(path=args.data_dir, name=args.train_keyphrase_set).toarray()
+
     R_train_keyphrase[R_train_keyphrase != 0] = 1
-    R_train_item_keyphrase = load_numpy(path=args.data_dir, name=args.train_item_keyphrase_set)
+    R_train_item_keyphrase = load_numpy(path=args.data_dir, name=args.train_item_keyphrase_set).T.toarray()
+
+    num_items, num_keyphrases = R_train_item_keyphrase.shape
+    for item in range(num_items):
+        item_keyphrase = R_train_item_keyphrase[item]
+        nonzero_keyphrases_index = item_keyphrase.nonzero()[0]
+        nonzero_keyphrases_frequency = item_keyphrase[nonzero_keyphrases_index]
+        candidate_index = nonzero_keyphrases_index[np.argsort(-nonzero_keyphrases_frequency)[:10]]
+        binarized_keyphrase = np.zeros(num_keyphrases)
+        binarized_keyphrase[candidate_index] = 1
+        R_train_item_keyphrase[item] = binarized_keyphrase
+
+    R_train_item_keyphrase = sparse.csr_matrix(R_train_item_keyphrase).T
 
     params = dict()
 #    params['model_saved_path'] = args.model_saved_path
 
     critiquing(R_train,
-               R_train_keyphrase.todense(),
+               R_train_keyphrase,
                R_train_item_keyphrase,
                params,
                args.num_users_sampled,
