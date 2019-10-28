1b00f177a25942c83694245cd65c700a572219f4
k9luo <lucas.luokai@gmail.com>
2019-10-27 23:50:06 -0400

    added critiquing models in utils model names

--- utils/modelnames.py
+++ utils/modelnames.py
@@ -2,5 +2,8 @@ from models.cdevae import cde_vae
 from models.e_cdevae import e_cde_vae
 
 
-models = {"CDE-VAE": cde_vae,"E-CDE-VAE": e_cde_vae}
+models = {"CDE-VAE": cde_vae,
+          "E-CDE-VAE": e_cde_vae}
+
+critiquing_models = {"E-CDE-VAE": e_cde_vae}
 
