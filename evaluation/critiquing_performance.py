94cc0bb7431a1b4e1c74d2b1b8fd5a2edfc0f51c
Kai Luo <lucas.luokai@gmail.com>
2019-10-30 22:09:27 -0400

    added plots for zero out energy

--- evaluation/critiquing_performance.py
+++ evaluation/critiquing_performance.py
@@ -8,14 +8,18 @@ import pandas as pd
 
 def critiquing_evaluation(train_set, keyphrase_train_set, item_keyphrase_train_set, model, model_name, num_users, num_items, num_users_sampled, topk):
     fmap_results = [[] for _ in topk]
+    np.random.seed(0)
     sampled_users = np.random.choice(num_users, num_users_sampled)
+    energy_df = pd.DataFrame()
+    print("sampled user: {}".format(sampled_users))
     for user in tqdm(sampled_users):
-        top_items_before_critique, top_items_after_critique, affected_items = critique_keyphrase(train_set,
+        top_items_before_critique, top_items_after_critique, affected_items, energy_df = critique_keyphrase(train_set,
                                                                                                  keyphrase_train_set,
                                                                                                  item_keyphrase_train_set,
                                                                                                  model,
                                                                                                  user,
                                                                                                  num_items,
+                                                                                                 energy_df,
                                                                                                  topk_keyphrase=10)
 
         all_items = np.array(range(num_items))
@@ -26,6 +30,10 @@ def critiquing_evaluation(train_set, keyphrase_train_set, item_keyphrase_train_s
                                                       np.isin(top_items_before_critique[:k], affected_items))
                                    - average_precisionk(top_items_after_critique[:k],
                                                         np.isin(top_items_after_critique[:k], affected_items)))
+    import ipdb; ipdb.set_trace()
+    from utils.io import save_dataframe_csv
+    save_dataframe_csv(energy_df, 'tables/beer_fmap_energy/', 'beer_fmap_energy.csv')
+    save_dataframe_csv(energy_df, 'tables/CDsVinyl_fmap_energy/', 'CDsVinyl_fmap_energy_beta_1.csv')
 
     fmap_results_dict = dict()
     for i, k in enumerate(topk):
--- utils/critique.py
+++ utils/critique.py
@@ -1,18 +1,23 @@
 import numpy as np
+import pandas as pd
 
 
-def critique_keyphrase(train_set, keyphrase_train_set, item_keyphrase_train_set, model, user_index, num_items, topk_keyphrase=10):
+def critique_keyphrase(train_set, keyphrase_train_set, item_keyphrase_train_set, model, user_index, num_items, energy_df, topk_keyphrase=10):
     # Get rating and explanation prediction for the given user and all item pairs
     rating, explanation = model.predict(train_set[user_index].todense())
+#    print(sum(explanation[0]))
+    energy_df = energy_df.append(pd.DataFrame([{"User": user_index, "Time": 1, "Energy": sum(explanation[0])}]))
 
     # For the given user, get top k keyphrases for that user
     explanation_rank_list = np.argsort(-explanation[0])[:topk_keyphrase]
 
+    keyphrase_index = []
     # Random critique one keyphrase among existing predicted keyphrases
-    keyphrase_index = int(np.random.choice(explanation_rank_list, 1)[0])
+    critiqued_keyphrase_index = int(np.random.choice(explanation_rank_list, 1)[0])
+    keyphrase_index.append(critiqued_keyphrase_index)
 
     # Get all affected items
-    affected_items = item_keyphrase_train_set[keyphrase_index].nonzero()[1]
+#    affected_items = item_keyphrase_train_set[keyphrase_index].nonzero()[1]
 
     """
     # Redistribute keyphrase prediction score
@@ -24,8 +29,27 @@ def critique_keyphrase(train_set, keyphrase_train_set, item_keyphrase_train_set,
     """
 
     explanation[0][keyphrase_index] = min(explanation[0])
+#    explanation[0][keyphrase_index] = -1
 
-    modified_rating, modified_explanation = model.refine_predict(train_set[user_index].todense(), explanation)
 
-    return np.argsort(rating.flatten())[::-1], np.argsort(modified_rating.flatten())[::-1], affected_items
+    for t in range(4):
+        modified_rating, modified_explanation = model.refine_predict(train_set[user_index].todense(), explanation)
+#        print(sum(modified_explanation[0]))
+        energy_df = energy_df.append(pd.DataFrame([{"User": user_index, "Time": t+2, "Energy": sum(modified_explanation[0])}]))
+
+
+        explanation_rank_list = np.argsort(-modified_explanation[0])[:topk_keyphrase]
+#        import ipdb; ipdb.set_trace()
+        explanation_rank_list = np.setdiff1d(explanation_rank_list, keyphrase_index)
+        critiqued_keyphrase_index = int(np.random.choice(explanation_rank_list, 1)[0])
+        keyphrase_index = np.append(keyphrase_index, critiqued_keyphrase_index)
+#        print(keyphrase_index)
+        keyphrase_index = np.unique(keyphrase_index)
+        affected_items = item_keyphrase_train_set[keyphrase_index].nonzero()[1]
+        modified_explanation[0][keyphrase_index] = min(modified_explanation[0])
+#        modified_explanation[0][keyphrase_index] = -1
+
+        explanation = modified_explanation
+
+    return np.argsort(rating.flatten())[::-1], np.argsort(modified_rating.flatten())[::-1], affected_items, energy_df
 
