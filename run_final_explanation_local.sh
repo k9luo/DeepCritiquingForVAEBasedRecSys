eacea30826cb90b255a0cda0f4a9305b7a4c07c8
k9luo <lucas.luokai@gmail.com>
2019-11-02 17:13:59 -0400

    added new bash script for local runs

---
+++ run_final_explanation_local.sh
@@ -0,0 +1,10 @@
+python reproduce_general_results.py --data_dir data/beer/ --tuning_result_path beer_explanation_tuning --save_path beer_explanation_final/beer_final_explanation_result1.csv --final_explanation
+python reproduce_general_results.py --data_dir data/beer/ --tuning_result_path beer_explanation_tuning --save_path beer_explanation_final/beer_final_explanation_result2.csv --final_explanation
+python reproduce_general_results.py --data_dir data/beer/ --tuning_result_path beer_explanation_tuning --save_path beer_explanation_final/beer_final_explanation_result3.csv --final_explanation
+python reproduce_general_results.py --data_dir data/beer/ --tuning_result_path beer_explanation_tuning --save_path beer_explanation_final/beer_final_explanation_result4.csv --final_explanation
+python reproduce_general_results.py --data_dir data/beer/ --tuning_result_path beer_explanation_tuning --save_path beer_explanation_final/beer_final_explanation_result5.csv --final_explanation
+python reproduce_general_results.py --data_dir data/CDsVinyl/ --tuning_result_path CDsVinyl_explanation_tuning --save_path CDsVinyl_explanation_final/CDsVinyl_final_explanation_result1.csv --final_explanation
+python reproduce_general_results.py --data_dir data/CDsVinyl/ --tuning_result_path CDsVinyl_explanation_tuning --save_path CDsVinyl_explanation_final/CDsVinyl_final_explanation_result2.csv --final_explanation
+python reproduce_general_results.py --data_dir data/CDsVinyl/ --tuning_result_path CDsVinyl_explanation_tuning --save_path CDsVinyl_explanation_final/CDsVinyl_final_explanation_result3.csv --final_explanation
+python reproduce_general_results.py --data_dir data/CDsVinyl/ --tuning_result_path CDsVinyl_explanation_tuning --save_path CDsVinyl_explanation_final/CDsVinyl_final_explanation_result4.csv --final_explanation
+python reproduce_general_results.py --data_dir data/CDsVinyl/ --tuning_result_path CDsVinyl_explanation_tuning --save_path CDsVinyl_explanation_final/CDsVinyl_final_explanation_result5.csv --final_explanation
