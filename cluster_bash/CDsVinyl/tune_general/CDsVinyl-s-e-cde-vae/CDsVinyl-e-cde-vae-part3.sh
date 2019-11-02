5a038aaf60f5f1c492b8e8f5ce65d4cc0ec3456c
hojinYang <hojin.yang7@gmail.com>
2019-11-01 21:04:30 -0400

    set hyperparameter

---
+++ cluster_bash/CDsVinyl/tune_general/CDsVinyl-s-e-cde-vae/CDsVinyl-e-cde-vae-part3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ~/ENV/bin/activate
+cd ~/Dual-Encoder
+python tune_parameters.py --data_dir data/CDsVinyl/ --save_path CDsVinyl_rating_tuning/s-e_cdevae_tuning_part3.csv --parameters config/CDsVinyl/s-e-cde-vae-tune-rating/s-e-cde-vae-part3.yml
---
+++ cluster_bash/CDsVinyl/tune_general/CDsVinyl-s-e-cde-vae/CDsVinyl-e-cde-vae-part4.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+source ~/ENV/bin/activate
+cd ~/Dual-Encoder
+python tune_parameters.py --data_dir data/CDsVinyl/ --save_path CDsVinyl_rating_tuning/s-e_cdevae_tuning_part4.csv --parameters config/CDsVinyl/s-e-cde-vae-tune-rating/s-e-cde-vae-part4.yml
+
